b0761e4bb0f668bda02d9d2ea7d66544b9970797
Haoxiang You <haoxiang.you@yale.edu>
2025-03-05 10:11:48 -0500

    add video_recorder

---
+++ viewer/video_recorder.py
@@ -0,0 +1,24 @@
+import imageio
+import os 
+
+class VideoRecorder:
+    def __init__(self, root_dir, fps=20, height=256, width=256, camera_id=0):
+        if root_dir is not None:
+            self.save_dir = root_dir
+            os.makedirs(self.save_dir, exist_ok=True)
+        else:
+            self.save_dir = None
+
+        self.fps = fps
+        self.render_kwargs = dict(height=height, width=width, camera_id=camera_id)
+        self.frames = []
+
+    def append(self, frame):
+        self.frames.append(frame)
+
+    def reset(self):
+        self.frames = []
+
+    def save(self, file_name):
+        path = os.path.join(self.save_dir, file_name)
+        imageio.mimsave(path, self.frames, fps=self.fps)
