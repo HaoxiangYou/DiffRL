483bab28a1147fc757f68c451e7de1084c7e96d0
Haoxiang You <haoxiang.you@yale.edu>
2025-03-27 13:35:52 -0400

    update play in dva, shac, so it will wait until video record is finished

--- algorithms/shac.py
+++ algorithms/shac.py
@@ -581,7 +581,7 @@ class SHAC:
         self.time_report.end_timer("algorithm")
 
         self.time_report.report()
-        
+
         self.save_time_report()
         
         self.save('final_policy')
@@ -624,7 +624,8 @@ class SHAC:
     def play(self, cfg):
         self.load(cfg['params']['general']['checkpoint'])
         self.run(cfg['params']['config']['player']['games_num'], save_dir=os.path.join(os.path.dirname(cfg['params']['general']['checkpoint']), "eval/play"))
-        
+        self.video_recorder.stop()
+
     def save(self, filename = None, save_dir = None):
         if save_dir is None:
             save_dir = self.log_dir
